fb15adf38968661104a618000ae8bf17e346e0d3
Mausumi134 <mausumighadei@gmail.com>
2026-02-05 00:28:02 +0530

    fix vite host blocking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -22,7 +22,8 @@ export default defineConfig({
   },
   preview: {
     port: process.env.PORT || 3000,
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: ['all']
   },
   build: {
     outDir: 'dist',
